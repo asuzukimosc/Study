4712b7dfba73b51be7347b432e69c178800a7c51
yuichi.tsunoda <yuichi.tsunoda@gmail.com>
2016-03-01 00:05:09 +0900

    Added "multiply2" function.

--- FromMathematicsToGenericProgramming/multiply.js
+++ FromMathematicsToGenericProgramming/multiply.js
@@ -1,5 +1,12 @@
 "use strict";
 
+let odd = function(n) {
+    return n & 0x1;
+};
+let half = function(n) {
+    return n >> 1;
+};
+
 exports.multiply0 = function(n, a) {
     if (n === 1) {
         return a;
@@ -9,13 +16,6 @@ exports.multiply0 = function(n, a) {
 }
 
 exports.multiply1 = function(n, a) {
-    let odd = function(n) {
-        return n & 0x1;
-    }
-    let half = function(n) {
-        return n >> 1;
-    }
-
     if (n === 1) {
         return a;
     } else {
@@ -31,4 +31,67 @@ exports.multiply_by_15 = function(a) {
     let b = (a + a) + a;
     let c = b + b;
     return (c + c) + b;
+}
+
+exports.mult_acc0 = function(r, n, a) {
+    if (n === 1) {
+        return r + a;
+    }
+    if (odd(n)) {
+        return this.mult_acc0(r + a, half(n), a + a);
+    } else {
+        return this.mult_acc0(r, half(n), a + a);
+    }
+}
+
+exports.mult_acc1 = function(r, n, a) {
+    if (n === 1) {
+        return r + a;
+    }
+    if (odd(n)) {
+        r = r + a;
+    }
+    return this.mult_acc1(r, half(n), a + a);
+}
+
+exports.mult_acc2 = function(r, n, a) {
+    if (odd(n)) {
+        r = r + a;
+        if (n === 1) {
+            return r;
+        }
+    }
+    return this.mult_acc2(r, half(n), a + a);
+}
+
+exports.mult_acc3 = function(r, n, a) {
+    if (odd(n)) {
+        r = r + a;
+        if (n === 1) {
+            return r;
+        }
+    }
+    n = half(n);
+    a = a + a;
+    return this.mult_acc3(r, n, a);
+}
+
+exports.mult_acc4 = function(r, n, a) {
+    while (true) {
+        if (odd(n)) {
+            r = r + a;
+            if (n === 1) {
+                return r;
+            }
+        }
+        n = half(n);
+        a = a + a;
+    }
+}
+
+exports.multiply2 = function(n, a) {
+    if (n === 1) {
+        return a;
+    }
+    return this.mult_acc4(a, n - 1, a);
 }
--- FromMathematicsToGenericProgramming/test/multiply.js
+++ FromMathematicsToGenericProgramming/test/multiply.js
@@ -18,3 +18,42 @@ describe('multiply_by_15', function() {
         assert.equal(multiply.multiply_by_15(3), 3 * 15);
     });
 });
+
+describe('multi_acc0', function() {
+    it('3 * 5', function() {
+        assert.equal(multiply.mult_acc0(0, 3, 5), 3 * 5);
+    });
+});
+
+describe('mult_acc1', function() {
+    it('3 * 5', function() {
+        assert.equal(multiply.mult_acc1(0, 3, 5), 3 * 5);
+    });
+});
+
+describe('mult_acc2', function() {
+    it('3 * 5', function() {
+        assert.equal(multiply.mult_acc2(0, 3, 5), 3 * 5);
+    });
+});
+
+describe('mult_acc3', function() {
+    it('3 * 5', function() {
+        assert.equal(multiply.mult_acc3(0, 3, 5), 3 * 5);
+    });
+});
+
+describe('mult_acc4', function() {
+    it('3 * 5', function() {
+        assert.equal(multiply.mult_acc4(0, 3, 5), 3 * 5);
+    });
+});
+
+describe('multily2', function() {
+    it('3 * 5', function() {
+        assert.equal(multiply.multiply2(3, 5), 3 * 5);
+    });
+    it('41 * 59', function() {
+        assert.equal(multiply.multiply1(41, 59), 41 * 59);
+    });
+});

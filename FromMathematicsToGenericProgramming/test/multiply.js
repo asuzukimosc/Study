9d03bbee26169ef503223f91b0d23e2b998a9905
yuichi.tsunoda <yuichi.tsunoda@gmail.com>
2016-03-02 00:26:00 +0900

    Refactoring multiply test code.

--- FromMathematicsToGenericProgramming/test/multiply.js
+++ FromMathematicsToGenericProgramming/test/multiply.js
@@ -1,17 +1,31 @@
 var assert = require('assert');
 var multiply = require('../multiply');
 
-describe('multiply0', function() {
-    it('3 * 5', function(){
-        assert.equal(multiply.multiply0(3, 5), 3 * 5);
+var test = function(func) {
+    var n = 3;
+    var a = 5;
+    var test_name = n + ' * ' + a;
+    var function_call = "multiply." + func + "(" + n + ", " + a + ")";
+    it(test_name, function() {
+        assert.equal(eval(function_call), n * a); 
     });
-});
+    
+    n = 41;
+    a = 59;
+    test_name = n + ' * ' + a;
+    function_call = "multiply." + func + "(" + n + ", " + a + ")";
+    it(test_name, function() {
+        assert.equal(eval(function_call), n * a); 
+    });
+}
 
-describe('multiply1', function() {
-    it('41 * 59', function() {
-        assert.equal(multiply.multiply1(41, 59), 41 * 59);
+var func = ['multiply0', 'multiply1', 'multiply2', 'multiply3', 'multiply4'];
+for (var i=0; i<func.length; i++) {
+    describe(func[i], function() {
+        test(func[i]);
     });
-});
+}
+
 
 describe('multiply_by_15', function() {
     it('3 * 15', function() {
@@ -46,32 +60,5 @@ describe('mult_acc3', function() {
 describe('mult_acc4', function() {
     it('3 * 5', function() {
         assert.equal(multiply.mult_acc4(0, 3, 5), 3 * 5);
-    });
-});
-
-describe('multily2', function() {
-    it('3 * 5', function() {
-        assert.equal(multiply.multiply2(3, 5), 3 * 5);
-    });
-    it('41 * 59', function() {
-        assert.equal(multiply.multiply1(41, 59), 41 * 59);
-    });
-});
-
-describe('multiply3', function() {
-    it('3 * 5', function() {
-        assert.equal(multiply.multiply3(3, 5), 3 * 5);
-    });
-    it('41 * 59', function() {
-        assert.equal(multiply.multiply3(41, 59), 41 * 59);
-    });
-});
-
-describe('multiply4', function() {
-    it('3 * 5', function() {
-        assert.equal(multiply.multiply4(3, 5), 3 * 5);
-    });
-    it('41 * 59', function() {
-        assert.equal(multiply.multiply4(41, 59), 41 * 59);
     });
 });
